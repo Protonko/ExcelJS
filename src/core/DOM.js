60124a0503d79a5473b41a1d01e5c4edbd4f852d
Egor Ermolaev <e.ermolaev@qsoft.ru>
2020-05-31 14:27:55 +0300

    Implementation methods for interaction DOM

---
+++ src/core/DOM.js
@@ -0,0 +1,39 @@
+class DOM {
+  constructor(selector) { // '' || {}
+    this.elem = typeof selector === 'string'
+      ? document.querySelector(selector)
+      : selector;
+  }
+
+  html(html) {
+    if (typeof html === 'string') {
+      this.elem.innerHTML = html;
+      return this;
+    } else {
+      return this.elem.outerHTML.trim();
+    }
+  }
+
+  clear() {
+    this.html('');
+    return this;
+  }
+
+  append(node) {
+    if (node instanceof DOM) node = node.elem;
+    this.elem.append(node.elem);
+    return this;
+  }
+}
+
+export function $(selector) {
+  return new DOM(selector);
+}
+
+$.create = (tagName, classNames = '') => {
+  const element = document.createElement(tagName);
+
+  if (classNames) element.classList.add(classNames);
+
+  return $(element);
+};
